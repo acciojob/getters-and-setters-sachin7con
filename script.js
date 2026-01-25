cc3bdb295594cb073a93bccd63db3015a0c78cb6
sachin7con <86023048+sachin7con@users.noreply.github.com>
2026-01-26 02:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-//complete this code
-class Person {}
+//SGN complete this code
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
+	get name() {
+		return this._name;
+	}
+	set age(newAge){
+		this._age = newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study() {
+		return `${this.name} is studying`;
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach() {
+		return `${this.name} is teaching`;
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
